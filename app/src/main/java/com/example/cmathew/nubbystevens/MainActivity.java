029982a77c1fe14b2237d18194d42499212062ca
cmathew <chris@deliv.co>
2018-02-02 18:27:36 -0800

    add floating action button to insert new car

--- app/src/main/java/com/example/cmathew/nubbystevens/MainActivity.java
+++ app/src/main/java/com/example/cmathew/nubbystevens/MainActivity.java
@@ -3,15 +3,15 @@ package com.example.cmathew.nubbystevens;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView inventoryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.inventoryList = findViewById(R.id.inventory_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
     }
 }
