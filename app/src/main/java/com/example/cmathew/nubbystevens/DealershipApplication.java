673359e88e986b64d9ae5d1a18f017f7b1004dfc
cmathew <chris.w.mathew@gmail.com>
2018-02-03 18:57:41 -0800

    refactor vehicle creation form

--- app/src/main/java/com/example/cmathew/nubbystevens/DealershipApplication.java
+++ app/src/main/java/com/example/cmathew/nubbystevens/DealershipApplication.java
@@ -4,16 +4,10 @@ import android.app.Activity;
 import android.app.Application;
 import android.support.v4.app.Fragment;
 
-import com.example.cmathew.nubbystevens.csv.VehicleParser;
 import com.example.cmathew.nubbystevens.database.DealershipDatabase;
 import com.example.cmathew.nubbystevens.di.component.ApplicationComponent;
 import com.example.cmathew.nubbystevens.di.component.DaggerApplicationComponent;
-import com.example.cmathew.nubbystevens.entity.Vehicle;
 import com.facebook.stetho.Stetho;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
 
 import javax.inject.Inject;
 
--- app/src/main/java/com/example/cmathew/nubbystevens/RegisterVehicleFragment.java
+++ app/src/main/java/com/example/cmathew/nubbystevens/RegisterVehicleFragment.java
@@ -3,11 +3,13 @@ package com.example.cmathew.nubbystevens;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -30,16 +32,37 @@ import com.example.cmathew.nubbystevens.entity.VehicleModel;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import dagger.android.support.AndroidSupportInjection;
 
 public class RegisterVehicleFragment extends DialogFragment {
     @Inject
     DealershipDatabase database;
 
-    private EditText vehicleMakeEntry;
-    private EditText vehicleModelEntry;
-    private EditText productionYearEntry;
-    private Button registerVehicleButton;
+    @BindView(R.id.vehicle_make_entry)
+    EditText vehicleMakeEntry;
+    @BindView(R.id.vehicle_make_layout)
+    TextInputLayout vehicleMakeLayout;
+
+    @BindView(R.id.vehicle_model_entry)
+    EditText vehicleModelEntry;
+    @BindView(R.id.vehicle_model_layout)
+    TextInputLayout vehicleModelLayout;
+
+    @BindView(R.id.vehicle_year_entry)
+    EditText productionYearEntry;
+    @BindView(R.id.vehicle_year_layout)
+    TextInputLayout productionYearLayout;
+
+    @BindView(R.id.register_vehicle_button)
+    Button registerVehicleButton;
+
+    @BindView(R.id.toolbar_register_vehicle)
+    Toolbar toolbar;
+
+    private Unbinder unbinder;
 
     public RegisterVehicleFragment() {
         // Required empty public constructor
@@ -83,24 +106,17 @@ public class RegisterVehicleFragment extends DialogFragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_register_vehicle, container, false);
 
+        this.unbinder = ButterKnife.bind(this, view);
         AppCompatActivity activity = (AppCompatActivity) getActivity();
-
-        Toolbar toolbar = view.findViewById(R.id.toolbar_register_vehicle);
         activity.setSupportActionBar(toolbar);
-
         ActionBar actionBar = activity.getSupportActionBar();
         setupToolbar(actionBar);
-
-        this.vehicleMakeEntry = view.findViewById(R.id.vehicle_make_entry);
-        this.vehicleModelEntry = view.findViewById(R.id.vehicle_model_entry);
-        this.productionYearEntry = view.findViewById(R.id.vehicle_year_entry);
-        this.registerVehicleButton = view.findViewById(R.id.register_vehicle_button);
 
         productionYearEntry.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == 100 || id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
-                    registerVehicle();
+                    requestVehicleRegistration();
                     return true;
                 }
                 return false;
@@ -110,29 +126,76 @@ public class RegisterVehicleFragment extends DialogFragment {
         registerVehicleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                registerVehicle();
+                requestVehicleRegistration();
             }
         });
 
         return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        unbinder.unbind();
+    }
+
+    private void requestVehicleRegistration() {
+        resetInputValidation();
+
+        boolean inputIsValid = validateInput();
+        if (!inputIsValid) {
+            return;
+        }
+
+        registerVehicle();
+        acknowledgeRegistration();
+    }
+
+    private boolean validateInput() {
+        final String requiredFieldError = getString(R.string.error_required_field);
+        String makeEntry = vehicleMakeEntry.getText().toString();
+        if (TextUtils.isEmpty(makeEntry)) {
+            vehicleMakeLayout.setError(requiredFieldError);
+            return false;
+        }
+
+        String modelEntry = vehicleModelEntry.getText().toString();
+        if (TextUtils.isEmpty(modelEntry)) {
+            vehicleModelLayout.setError(requiredFieldError);
+            return false;
+        }
+
+        String yearEntry = productionYearEntry.getText().toString();
+        if (TextUtils.isEmpty(yearEntry)) {
+            productionYearLayout.setError(requiredFieldError);
+            return false;
+        }
+
+        return true;
+    }
+
     private void registerVehicle() {
         String makeEntry = vehicleMakeEntry.getText().toString();
         String modelEntry = vehicleModelEntry.getText().toString();
         String yearEntry = productionYearEntry.getText().toString();
-        int productionYear = Integer.parseInt(yearEntry);
 
+        int productionYear = Integer.parseInt(yearEntry);
         VehicleCreator creator = new VehicleCreator(database);
         creator.insertVehicle(makeEntry, modelEntry, productionYear);
-
-        acknowledgeRegistration();
     }
 
     private void acknowledgeRegistration() {
         Toast.makeText(getActivity(), "Vehicle Registered!", Toast.LENGTH_SHORT).show();
         dismiss();
     }
+
+    private void resetInputValidation() {
+        vehicleMakeEntry.setError(null);
+        vehicleModelEntry.setError(null);
+        productionYearEntry.setError(null);
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
